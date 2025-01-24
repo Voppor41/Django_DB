4d3267741b04040c046fa9afbc58be6d66dbdfdf
Voppor41 <egorselifanov334@yandex.com>
2025-01-24 16:11:51 +0300

    Добавлена админка

--- newapp/task1/admin.py
+++ newapp/task1/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import *
+
+
 # Register your models here.
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    search_fields = ('title', )
+    list_filter = ('size', 'cost')
+    list_display = ('title', 'cost', 'size', )
+    list_per_page = 20
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_filter = ('balance', 'age', )
+    list_display = ('name', 'balance', 'age', )
+    search_fields = ('name', )
+    list_per_page = 30
+    readonly_fields = ('balance', )
